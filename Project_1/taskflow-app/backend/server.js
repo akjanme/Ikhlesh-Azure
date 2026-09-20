2591aeecffbf046d33ee85ab0d3955c16ef13a12
akjanme <akjanme@hotmail.com>
2026-09-20 18:25:13 +0530

    updated ui and app service

--- Project_1/taskflow-app/backend/server.js
+++ Project_1/taskflow-app/backend/server.js
@@ -1,21 +1,19 @@
 const express = require("express");
-const cors = require('cors');
+const cors = require("cors");
 
 const weatherRoutes = require("./routes/weatherRoutes");
 const userRoutes = require("./routes/userRoutes");
 
- 
 const app = express();
+
 app.use(cors());
-
-
-const PORT = process.env.PORT || 3000;
-
 app.use(express.json());
 
 app.use("/api/weather", weatherRoutes);
 app.use("/api/users", userRoutes);
 
-app.listen(PORT, () => {
-    console.log(`Server running at http://localhost:${PORT}`);
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, "0.0.0.0", () => {
+    console.log(`Server running on port ${PORT}`);
 });
